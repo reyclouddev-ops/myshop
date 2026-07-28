33094d91eae633bf5c387c86a2c68e8062f1fc7c
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-28 18:23:22 +0700

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req){
+
+const token=req.cookies.get("token");
+
+if(
+
+req.nextUrl.pathname.startsWith("/admin")
+
+&&
+
+!token
+
+){
+
+return NextResponse.redirect(
+
+new URL("/login",req.url)
+
+);
+
+}
+
+return NextResponse.next();
+
+}
+
+export const config={
+
+matcher:["/admin/:path*"]
+
+}
