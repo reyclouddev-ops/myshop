2613be11839ab1aa4c0d914ac4d0790fecbaa5bf
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-28 17:37:54 +0700

    Create robux.js

---
+++ data/robux.js
@@ -0,0 +1,25 @@
+const robux = [
+{ nama:"240 Robux", harga:"Rp47.970" },
+{ nama:"320 Robux", harga:"Rp63.960" },
+{ nama:"500 Robux", harga:"Rp79.700" },
+{ nama:"1000 Robux", harga:"Rp159.400" },
+{ nama:"1500 Robux", harga:"Rp239.100" },
+{ nama:"2000 Robux", harga:"Rp318.800" },
+{ nama:"2500 Robux", harga:"Rp398.500" },
+{ nama:"3000 Robux", harga:"Rp478.200" },
+{ nama:"3500 Robux", harga:"Rp557.900" },
+{ nama:"4000 Robux", harga:"Rp637.600" },
+{ nama:"5000 Robux", harga:"Rp797.000" },
+{ nama:"6000 Robux", harga:"Rp956.400" },
+{ nama:"7000 Robux", harga:"Rp1.115.800" },
+{ nama:"8000 Robux", harga:"Rp1.275.200" },
+{ nama:"9000 Robux", harga:"Rp1.434.600" },
+{ nama:"10000 Robux", harga:"Rp1.594.000" },
+{ nama:"17000 Robux", harga:"Rp2.630.000" },
+{ nama:"20000 Robux", harga:"Rp3.188.000" },
+{ nama:"30000 Robux", harga:"Rp4.782.000" },
+{ nama:"40000 Robux", harga:"Rp6.376.000" },
+{ nama:"50000 Robux", harga:"Rp9.675.000" }
+]
+
+export default robux
