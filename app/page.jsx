d0c3d8df7ce58889e1b9d9ba3d07aa96fa8f33c5
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-28 17:58:58 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,19 @@
 import Navbar from "@/components/Navbar";
 import Banner from "@/components/Banner";
+import SearchBar from "@/components/SearchBar";
+import PromoBanner from "@/components/PromoBanner";
 import Category from "@/components/Category";
-import Footer from "@/components/Footer";
+import BottomNav from "@/components/BottomNav";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <SearchBar />
       <Banner />
+      <PromoBanner />
       <Category />
-      <Footer />
+      <BottomNav />
     </>
   );
 }
