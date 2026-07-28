feecab62819ec1524e27412bf885b6099de956c7
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-28 17:35:54 +0700

    Create Banner.jsx

---
+++ components/Banner.jsx
@@ -0,0 +1,21 @@
+export default function Banner(){
+
+return(
+
+<div className="banner">
+
+<h1>
+Selamat Datang di ReyCloudShop
+</h1>
+
+<p>
+
+Panel • VPS • Domain • Robux • GamePass
+
+</p>
+
+</div>
+
+)
+
+}
