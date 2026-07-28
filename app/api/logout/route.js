bea7636d79b793ba0e32b824d00cbc2ed15936df
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-28 18:45:38 +0700

    Create route.js

---
+++ app/api/logout/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+export async function POST(){
+
+const res=NextResponse.json({
+success:true
+});
+
+res.cookies.set("token","",{
+maxAge:0,
+path:"/"
+});
+
+return res;
+
+}
