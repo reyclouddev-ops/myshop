ad5948be336807eb4f699362cbae13d9f83fa2fc
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-28 17:55:55 +0700

    Create SearchBar.jsx

---
+++ components/SearchBar.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Search } from "lucide-react";
+
+export default function SearchBar(){
+
+return(
+
+<div className="searchBar">
+
+<Search/>
+
+<input
+
+placeholder="Cari Produk..."
+
+ />
+
+</div>
+
+)
+
+}
