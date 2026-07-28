e7d3c268d1e89613183462367df86cffc1864ae5
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-28 17:38:17 +0700

    Create robuxDelay.js

---
+++ data/robuxDelay.js
@@ -0,0 +1,23 @@
+const robuxDelay = [
+
+{ nama:"100 Robux", harga:"Rp11.990" },
+{ nama:"200 Robux", harga:"Rp23.980" },
+{ nama:"300 Robux", harga:"Rp35.970" },
+{ nama:"400 Robux", harga:"Rp47.960" },
+{ nama:"500 Robux", harga:"Rp59.950" },
+{ nama:"600 Robux", harga:"Rp71.940" },
+{ nama:"700 Robux", harga:"Rp83.930" },
+{ nama:"800 Robux", harga:"Rp95.920" },
+{ nama:"900 Robux", harga:"Rp107.910" },
+{ nama:"1000 Robux", harga:"Rp119.900" },
+{ nama:"2000 Robux", harga:"Rp239.800" },
+{ nama:"3000 Robux", harga:"Rp359.700" },
+{ nama:"4000 Robux", harga:"Rp479.600" },
+{ nama:"5000 Robux", harga:"Rp599.500" },
+{ nama:"10000 Robux", harga:"Rp1.199.000" },
+{ nama:"17000 Robux", harga:"Rp2.038.300" },
+{ nama:"20000 Robux", harga:"Rp2.398.000" }
+
+]
+
+export default robuxDelay
