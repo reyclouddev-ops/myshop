854ebbe54f4d52802b86c085852e3e468c06b047
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-28 20:32:50 +0700

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,88 +2,65 @@
 
 import { useState } from "react";
 
-export default function Login(){
+export default function Login() {
 
-const[username,setUsername]=useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-const[password,setPassword]=useState("");
+  async function login() {
 
-async function login(){
+    const res = await fetch("/api/login", {
 
-const res=await fetch("/api/login",{
+      method: "POST",
 
-method:"POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
 
-headers:{
+      body: JSON.stringify({
+        username,
+        password
+      })
 
-"Content-Type":"application/json"
+    });
 
-},
+    const data = await res.json();
 
-body:JSON.stringify({
+    if (!data.success) {
+      return alert(data.message);
+    }
 
-username,
+    if (data.success) {
+      location.href = "/admin";
+    }
 
-password
+  } 
 
-})
+  return (
 
-});
+    <div className="checkout">
 
-const data=await res.json();
+      <h1>
+        Login Admin
+      </h1>
 
-if(!data.success){
+      <input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
 
-return alert(data.message);
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
-}
+      <button onClick={login}>
+        Login
+      </button>
 
-if(data.success){
+    </div>
 
-location.href="/admin";
-
-}
-
-return(
-
-<div className="checkout">
-
-<h1>
-
-Login Admin
-
-</h1>
-
-<input
-
-placeholder="Username"
-
-onChange={(e)=>setUsername(e.target.value)}
-
-/>
-
-<input
-
-type="password"
-
-placeholder="Password"
-
-onChange={(e)=>setPassword(e.target.value)}
-
-/>
-
-<button
-
-onClick={login}
-
->
-
-Login
-
-</button>
-
-</div>
-
-)
+  );
 
 }
