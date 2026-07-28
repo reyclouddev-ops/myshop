5eb624677b31afc1b3f570ec46c360e7cbd375b5
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-28 17:39:17 +0700

    Create panel.js

---
+++ data/panel.js
@@ -0,0 +1,17 @@
+const panel=[
+
+{ nama:"Panel 1GB", harga:"Rp1.500" },
+{ nama:"Panel 2GB", harga:"Rp2.500" },
+{ nama:"Panel 3GB", harga:"Rp3.500" },
+{ nama:"Panel 4GB", harga:"Rp4.500" },
+{ nama:"Panel 5GB", harga:"Rp5.500" },
+{ nama:"Panel 6GB", harga:"Rp6.500" },
+{ nama:"Panel 7GB", harga:"Rp7.500" },
+{ nama:"Panel 8GB", harga:"Rp8.500" },
+{ nama:"Panel 9GB", harga:"Rp9.500" },
+{ nama:"Panel 10GB", harga:"Rp10.500" },
+{ nama:"Unlimited", harga:"Rp15.500" }
+
+]
+
+export default panel
