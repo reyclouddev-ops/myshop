5a9fa00c19a7d85fb7973aafefbfc597bd9fca02
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-28 17:34:59 +0700

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+import Banner from "@/components/Banner";
+import Category from "@/components/Category";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <Category />
+      <Footer />
+    </>
+  );
+}
