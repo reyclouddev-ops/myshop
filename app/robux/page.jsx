8cc9c485b6ef149607cc16698c71b188fc736a3e
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-28 17:47:48 +0700

    Update page.jsx

--- app/robux/page.jsx
+++ app/robux/page.jsx
@@ -1,42 +1,21 @@
-import robux from "@/data/robux"
-import ProductCard from "@/components/ProductCard"
+import robux from "@/data/robux";
+import ProductCard from "@/components/ProductCard";
 
-export default function Robux(){
+export default function Robux() {
+  return (
+    <div className="container">
+      <h1>⚡ Robux Instant</h1>
 
-return(
-
-<div className="container">
-
-<h1>
-
-⚡ Robux Instant
-
-</h1>
-
-<div className="grid">
-
-{
-
-robux.map((item,index)=>(
-
-<ProductCard
-
-key={index}
-
-nama={item.nama}
-
-harga={item.harga}
-
-/>
-
-))
-
-}
-
-</div>
-
-</div>
-
-)
-
+      <div className="grid">
+        {robux.map((item, index) => (
+          <ProductCard
+            key={index}
+            nama={item.nama}
+            harga={item.harga}
+            kategori="robux"
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
