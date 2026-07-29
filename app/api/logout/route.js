9f8f5fb7ad01592b53198552cdd5b5d9276fa175
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-29 11:59:17 +0700

    Update route.js

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -1,16 +1,14 @@
 import { NextResponse } from "next/server";
 
-export async function POST(){
+export async function POST() {
+  const res = NextResponse.json({
+    success: true
+  });
 
-const res=NextResponse.json({
-success:true
-});
+  res.cookies.set("admin", "", {
+    path: "/",
+    maxAge: 0
+  });
 
-res.cookies.set("token","",{
-maxAge:0,
-path:"/"
-});
-
-return res;
-
+  return res;
 }
