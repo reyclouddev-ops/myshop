e8bbcdce8be7518ece550ba0087f21932a7a8051
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-29 11:58:44 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,19 +1,10 @@
 import { NextResponse } from "next/server";
-import { verifyToken } from "@/lib/auth";
 
 export function middleware(req) {
-  const token = req.cookies.get("token")?.value;
+  const admin = req.cookies.get("admin")?.value;
 
-  if (!token) {
+  if (admin !== "true") {
     return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  const user = verifyToken(token);
-
-  if (!user) {
-    const res = NextResponse.redirect(new URL("/login", req.url));
-    res.cookies.delete("token");
-    return res;
   }
 
   return NextResponse.next();
